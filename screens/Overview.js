fecab15d6d93ade6443ef66723866c73cbb839ba
Ram Simran <garemella.ramsimran@gmail.com>
2020-01-14 13:14:52 +0530

    Small issues fixed

--- screens/Overview.js
+++ screens/Overview.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, Image, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import {
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet
+} from "react-native";
 
-import { Block, Card, Text, Icon, Label } from '../components';
-import * as theme from '../constants/theme';
+import { Block, Card, Text, Icon, Label } from "../components";
+import * as theme from "../constants/theme";
 
 const styles = StyleSheet.create({
   overview: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: theme.colors.white,
+    flexDirection: "column",
+    backgroundColor: theme.colors.white
   },
   margin: {
-    marginHorizontal: 25,
+    marginHorizontal: 25
   },
   driver: {
-    marginBottom: 11,
+    marginBottom: 11
   },
   avatar: {
     width: 48,
     height: 48,
-    borderRadius: 24,
+    borderRadius: 24
   }
 });
 
@@ -31,16 +37,22 @@ class Overview extends Component {
     headerRightContainerStyle: {
       paddingRight: 24
     },
-    headerLeft: (
-      <TouchableOpacity><Icon menu /></TouchableOpacity>
+    headerLeft: () => (
+      <TouchableOpacity>
+        <Icon menu />
+      </TouchableOpacity>
     ),
-    headerRight: (
-      <TouchableOpacity><Icon notification /></TouchableOpacity>
+    headerRight: () => (
+      <TouchableOpacity>
+        <Icon notification />
+      </TouchableOpacity>
     ),
-    headerTitle: (
-      <Block row middle><Text h4>Overview</Text></Block>
+    headerTitle: () => (
+      <Block row middle>
+        <Text h4>Overview</Text>
+      </Block>
     )
-  }
+  };
 
   render() {
     return (
@@ -48,15 +60,22 @@ class Overview extends Component {
         <ScrollView contentContainerStyle={{ paddingVertical: 25 }}>
           <Card row middle style={styles.margin}>
             <Block flex={1.2} center middle style={{ marginRight: 20 }}>
-              <Text light height={43} size={36} spacing={-0.45}>86</Text>
-              <Text ligth caption center style={{ paddingHorizontal: 16, marginTop: 3 }}>
+              <Text light height={43} size={36} spacing={-0.45}>
+                86
+              </Text>
+              <Text
+                ligth
+                caption
+                center
+                style={{ paddingHorizontal: 16, marginTop: 3 }}
+              >
                 OPERATING SCORE
               </Text>
             </Block>
             <Block>
               <Text paragraph color="black3">
-                All cars are operating well.
-                There were 1,233 trips since your last login.
+                All cars are operating well. There were 1,233 trips since your
+                last login.
               </Text>
             </Block>
           </Card>
@@ -64,14 +83,22 @@ class Overview extends Component {
           <Block row style={[styles.margin, { marginTop: 18 }]}>
             <Card middle style={{ marginRight: 7 }}>
               <Icon vehicle />
-              <Text h2 style={{ marginTop: 17 }}>1,428</Text>
-              <Text paragraph color="gray">Vehicles on track</Text>
+              <Text h2 style={{ marginTop: 17 }}>
+                1,428
+              </Text>
+              <Text paragraph color="gray">
+                Vehicles on track
+              </Text>
             </Card>
-            
+
             <Card middle style={{ marginLeft: 7 }}>
               <Icon distance />
-              <Text h2 style={{ marginTop: 17 }}>158.3</Text>
-              <Text paragraph color="gray">Distance driven</Text>
+              <Text h2 style={{ marginTop: 17 }}>
+                158.3
+              </Text>
+              <Text paragraph color="gray">
+                Distance driven
+              </Text>
             </Card>
           </Block>
 
@@ -82,11 +109,15 @@ class Overview extends Component {
             <Block row right>
               <Block flex={2} row center right>
                 <Label blue />
-                <Text paragraph color="gray">Today</Text>
+                <Text paragraph color="gray">
+                  Today
+                </Text>
               </Block>
               <Block row center right>
                 <Label purple />
-                <Text paragraph color="gray">Yesterday</Text>
+                <Text paragraph color="gray">
+                  Yesterday
+                </Text>
               </Block>
             </Block>
             <Block>
@@ -94,26 +125,32 @@ class Overview extends Component {
             </Block>
           </Card>
 
-          <Card
-            title="TOP DRIVERS"
-            style={[styles.margin, { marginTop: 18 }]}
-          >
+          <Card title="TOP DRIVERS" style={[styles.margin, { marginTop: 18 }]}>
             <Block style={styles.driver}>
               <TouchableOpacity activeOpacity={0.8}>
                 <Block row center>
                   <Block>
                     <Image
                       style={styles.avatar}
-                      source={{ uri: 'https://images.unsplash.com/photo-1506244856291-8910ea843e81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80' }}
+                      source={{
+                        uri:
+                          "https://images.unsplash.com/photo-1506244856291-8910ea843e81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                      }}
                     />
                   </Block>
                   <Block flex={2}>
                     <Text h4>Grand Tesoro</Text>
-                    <Text paragraph color="gray">Chevrolet Bolt</Text>
+                    <Text paragraph color="gray">
+                      Chevrolet Bolt
+                    </Text>
                   </Block>
                   <Block>
-                    <Text paragraph right color="black">$6,432</Text>
-                    <Text paragraph right color="gray">1,232 miles</Text>
+                    <Text paragraph right color="black">
+                      $6,432
+                    </Text>
+                    <Text paragraph right color="gray">
+                      1,232 miles
+                    </Text>
                   </Block>
                 </Block>
               </TouchableOpacity>
@@ -124,16 +161,25 @@ class Overview extends Component {
                   <Block>
                     <Image
                       style={styles.avatar}
-                      source={{ uri: 'https://images.unsplash.com/photo-1521657249896-063c0c611fe5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80' }}
+                      source={{
+                        uri:
+                          "https://images.unsplash.com/photo-1521657249896-063c0c611fe5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                      }}
                     />
                   </Block>
                   <Block flex={2}>
                     <Text h4>Invision App</Text>
-                    <Text paragraph color="gray">Tesla Model X</Text>
+                    <Text paragraph color="gray">
+                      Tesla Model X
+                    </Text>
                   </Block>
                   <Block>
-                    <Text paragraph right color="black">$6,432</Text>
-                    <Text paragraph right color="gray">1,232 miles</Text>
+                    <Text paragraph right color="black">
+                      $6,432
+                    </Text>
+                    <Text paragraph right color="gray">
+                      1,232 miles
+                    </Text>
                   </Block>
                 </Block>
               </TouchableOpacity>
@@ -144,22 +190,30 @@ class Overview extends Component {
                   <Block>
                     <Image
                       style={styles.avatar}
-                      source={{ uri: 'https://images.unsplash.com/photo-1536700503339-1e4b06520771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80' }}
+                      source={{
+                        uri:
+                          "https://images.unsplash.com/photo-1536700503339-1e4b06520771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                      }}
                     />
                   </Block>
                   <Block flex={2}>
                     <Text h4>React UI Kit</Text>
-                    <Text paragraph color="gray">Volvo Intellisafe</Text>
+                    <Text paragraph color="gray">
+                      Volvo Intellisafe
+                    </Text>
                   </Block>
                   <Block>
-                    <Text paragraph right color="black">$6,432</Text>
-                    <Text paragraph right color="gray">1,232 miles</Text>
+                    <Text paragraph right color="black">
+                      $6,432
+                    </Text>
+                    <Text paragraph right color="gray">
+                      1,232 miles
+                    </Text>
                   </Block>
                 </Block>
               </TouchableOpacity>
             </Block>
           </Card>
-
 
           <Card
             title="TRIPS BY TYPE"
@@ -170,24 +224,32 @@ class Overview extends Component {
             </Block>
             <Block row space="between" style={{ marginTop: 25 }}>
               <Block>
-                <Text h2 light>1,744</Text>
+                <Text h2 light>
+                  1,744
+                </Text>
                 <Block row center>
                   <Label blue />
-                  <Text paragraph color="gray">Confort</Text>
+                  <Text paragraph color="gray">
+                    Confort
+                  </Text>
                 </Block>
               </Block>
               <Block>
-                <Text h2 light>2,312</Text>
+                <Text h2 light>
+                  2,312
+                </Text>
                 <Block row center>
                   <Label purple />
-                  <Text paragraph color="gray">Premium</Text>
+                  <Text paragraph color="gray">
+                    Premium
+                  </Text>
                 </Block>
               </Block>
             </Block>
           </Card>
         </ScrollView>
       </SafeAreaView>
-    )
+    );
   }
 }
 
